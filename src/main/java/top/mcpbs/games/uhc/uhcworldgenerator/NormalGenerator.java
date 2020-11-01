deae92db84fae7422c862e29a0c2117add845c98
smart_cmd <3523206925@qq.com>
2020-11-01 14:46:31 +0800

    override

--- src/main/java/top/mcpbs/games/uhc/uhcworldgenerator/NormalGenerator.java
+++ src/main/java/top/mcpbs/games/uhc/uhcworldgenerator/NormalGenerator.java
@@ -161,13 +161,13 @@ public class NormalGenerator extends VanillaGenerator {
 
         this.populators = ImmutableList.of(
                 new PopulatorOre(STONE, new OreType[]{
-                        new OreType(Block.get(COAL_ORE), 20 * 20, 17, 0, 128),
-                        new OreType(Block.get(IRON_ORE), 20 * 20, 9, 0, 64),
-                        new OreType(Block.get(REDSTONE_ORE), 8 * 20, 8, 0, 16),
-                        new OreType(Block.get(LAPIS_ORE), 8 * 20, 7, 0, 16),
-                        new OreType(Block.get(GOLD_ORE), 16 * 20, 9, 0, 32),
-                        new OreType(Block.get(DIAMOND_ORE), 4 * 20, 8, 0, 16),
-                        new OreType(Block.get(EMERALD_ORE), 4 * 20, 8, 0, 16),
+                        new OreType(Block.get(COAL_ORE), 20 * 30, 17, 0, 128),
+                        new OreType(Block.get(IRON_ORE), 20 * 30, 9, 0, 64),
+                        new OreType(Block.get(REDSTONE_ORE), 8 * 30, 8, 0, 16),
+                        new OreType(Block.get(LAPIS_ORE), 8 * 30, 7, 0, 16),
+                        new OreType(Block.get(GOLD_ORE), 16 * 30, 9, 0, 32),
+                        new OreType(Block.get(DIAMOND_ORE), 4 * 30, 8, 0, 16),
+                        new OreType(Block.get(EMERALD_ORE), 4 * 30, 8, 0, 16),
                         new OreType(Block.get(DIRT), 10, 33, 0, 128),
                         new OreType(Block.get(GRAVEL), 8 * 10, 33, 0, 128),
                         new OreType(Block.get(STONE, BlockStone.GRANITE), 10, 33, 0, 80),
